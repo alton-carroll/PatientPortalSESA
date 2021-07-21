63a6203620d29cea15349f0e89a9144ce4bdbf5d
Alton Carroll <alton.carroll@outlook.com>
2021-07-21 16:51:27 -0500

    Patient data source added

---
+++ PatientPortalSESA/SiteAssets/Patients.js
@@ -0,0 +1,32 @@
+﻿var patients = [{
+    ID: 1,
+    FirstName: "John",
+    LastName: "Smith",
+    Title: "Mr.",
+    Street: "123 Main Street",
+    City: "Austin",
+    State: "Texas",
+    Zip: "123456"
+
+}, {
+    ID: 2,
+    FirstName: "Jane",
+    LastName: "Johnson",
+    Title: "Ms.",
+    Street: "987 Pine Avenue",
+    City: "Dallas",
+    State: "Texas",
+    Zip: "123456"
+
+}, {
+    ID: 3,
+    FirstName: "Pat",
+    LastName: "Smithers",
+    Title: "Mrs.",
+    Street: "569 Spring Loop",
+    City: "Waco",
+    State: "Texas",
+    Zip: "123456"
+
+},
+];
